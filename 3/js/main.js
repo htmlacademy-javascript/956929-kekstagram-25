171aef50520c12a991c34a5fd425c39163318544
Keks <keks@htmlacademy.ru>
2022-04-14 18:19:21 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/main.js
@@ -0,0 +1,8 @@
+function Random (a, b) {
+  const down = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const up = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+
+  const result = Math.random() * (up - down + 1) + up;
+
+  return Math.floor(result);
+}
